3cafaf6b898a7ba19b0379d5887334685321b336
dungdung <seasun9376@gmail.com>
2023-02-08 10:57:40 +0900

    쿵쿵따 타이머 및 css 마무리

--- js/main.js
+++ js/main.js
@@ -5,10 +5,15 @@ const timeWrap = document.querySelector('.timeWrap');
 const playerArr = getQuery();
 const player = [];
 let i = 0;
+//입력칸 포커스
+wordZone.focus();
 //사용자 이름 배열에 넣기
 for(i=0; i<Number(playerArr.persons); i++){
     player.push(playerArr['name'+i]);
 }
+
+timer()
+
 //시작 player
 playerText.textContent = player[0] + '님 차례입니다';
 wordZone.addEventListener('keypress',function(e){
@@ -21,7 +26,8 @@ inputBtn.addEventListener('click',function(){
 })
 //i 초기화
 i = 0;
-function wordFct(){//구
+function wordFct(){
+    wordZone.focus();
     //player 추가하기
     i = i + 1;
     if(i < Number(playerArr.persons)){
@@ -37,6 +43,7 @@ function wordFct(){//구
         wordRecord.textContent = keyword;
     } else { //키워드가 있을때
         let keyword = wordZone.value;
+        console.log(keyword);
         let lastRecord = wordRecordText;
         let lastKeyword = lastRecord.substr(lastRecord.length-1,1); //이전사람 마지막 단어
         let firstLKeyword = keyword.substr(0,1); //현재 첫번째 단어
@@ -44,9 +51,12 @@ function wordFct(){//구
             wordRecord.textContent = keyword;
         } else {
             alert('틀렸습니다');
-            reset();
+            timer();
+            window.location.href = window.origin + '/goOnWording/index.html';
         }
     }
+    timer();
+    wordZone.value = null;
 }
 //인풋값 클릭시 value 초기화
 wordZone.addEventListener('click',function(){
@@ -73,3 +83,28 @@ function reset(){
 
 }
 
+//타이머 function
+function timer(){
+    //타이머 제작하기(제한시간 10초);
+    let t = 10;
+    timeWrap.innerHTML = '남은시간<em>' + t + '</em>초';
+    const timeHandler = function(){
+        const timeWrap = document.querySelector('.timeWrap');
+        const wordZone = document.querySelector('#wordZone');
+        t = t -1;
+        timeWrap.innerHTML = '남은시간<em>' + t + '</em>초';
+        if(t < 0) {
+            if(wordZone.value == ''){
+                alert('GAME OVER');
+                clearInterval(timer);
+                window.location.href = window.origin + '/goOnWording/index.html';
+            } else {
+                clearInterval(timer);
+            }
+        }
+        if(wordZone.value != ''){
+            clearInterval(timer);
+        }
+    };
+    const timer = setInterval(timeHandler,1000);
+}
